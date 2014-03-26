d830ead35824a20d8e9658db50e8c0f5616e5992
Andrew Tremblay <gloriousrocketship@gmail.com>
2014-03-25 18:47:16 -0700

    adding AssetLibrary framework to grab image metadata.

--- Screenshots/Screenshots/ADTViewController.m
+++ Screenshots/Screenshots/ADTViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ADTViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+
 
 @interface ADTViewController ()
 @end
@@ -16,6 +18,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    // Get the assets library
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    
+    // Enumerate just the photos and videos group by using ALAssetsGroupSavedPhotos.
+    [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos
+                           usingBlock:^(ALAssetsGroup *group, BOOL *stop)
+     {
+         
+         // Within the group enumeration block, filter to enumerate just photos.
+         [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+         
+         // For this example, we're only interested in the first item.
+         [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:0]
+                                 options:0
+                              usingBlock:^(ALAsset *alAsset, NSUInteger index, BOOL *innerStop)
+          {
+              
+              // The end of the enumeration is signaled by asset == nil.
+              if (alAsset) {
+                  ALAssetRepresentation *representation = [alAsset defaultRepresentation];
+                  NSDictionary *imageMetadata = [representation metadata];
+                  // Do something interesting with the metadata.
+                  NSLog(@"Image Metadata! %@", imageMetadata);
+              }
+          }];
+     }
+                         failureBlock: ^(NSError *error)
+     {
+         // Typically you should handle an error more gracefully than this.
+         NSLog(@"No groups");
+     }];
 
 	// Do any additional setup after loading the view, typically from a nib.
 }
